2bc3570d94fd83f0e76219334958e62d625f871b
squarfiuz <squarfiuz@example.com>
2022-02-26 09:28:54 +0100

    Update commands

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
   description: 'Affiche le ping du bot.',
   type: 1,
-  async run({ interaction }) {
-    interaction.reply('Pong');
+  async run({ client, interaction }) {
+    interaction.reply(`:ping_pong: Pong ! Gateway: \`${client.ws.ping}ms\`.`);
   }
 };
