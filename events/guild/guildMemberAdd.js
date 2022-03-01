d521b9613c05107aac6cda54e5c8e8196d484ccc
squarfiuz <squarfiuz@example.com>
2022-03-01 16:34:51 +0100

    Fix role bug

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,4 +1,4 @@
 module.exports = async (client, member) => {
-  member.roles.add(client.autorole_roles, 'Ajout des autoroles.');
+  member.roles.add(client.config.autorole_roles, 'Ajout des autoroles.');
   await client.pool.query(`INSERT INTO users(id, credits, experience, level) VALUES (${member.id}, 0, 0, 1)`);
 };
